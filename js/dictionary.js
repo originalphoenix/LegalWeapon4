e6eb0f8568b3571ff44c7865d7d7b1193a1a50db
Samantha Christoff <Samantha@Samantha-Christoffs-MacBook-Pro.local>
2014-12-06 15:47:05 -0700

    seperating the dictionary out into it's own file

---
+++ js/dictionary.js
@@ -0,0 +1,14 @@
+/*
+ * Dictionary File
+ *
+ */
+
+var dict = {
+  "TERMS OF SERVICE": {
+    hl: "Terms of Lol"
+  },
+  "Download plugin": {
+    pt: "Descarregar plugin",
+    en: "Download plugin"
+  }
+}
